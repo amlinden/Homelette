ce98ce690c65da1121c81661b9f358723e2f1b9d
Alixred <alexaj@kth.se>
2017-02-10 10:56:52 +0100

    added views to app.js

--- js/app.js
+++ js/app.js
@@ -1,8 +1,16 @@
+//kontrollerar views och kontrollers och laddar DinnerModel
 $(function() {
 	//We instantiate our model
 	var model = new DinnerModel();
 	
 	//And create the needed controllers and views
 	var exampleView = new ExampleView($("#exampleView"));
+
+	var HomeScreen = new HomeScreen($("#HomeScreen"));
+	var LeftNavBar = new LeftNavBar($("#LeftNavBar"));
+	var SelectDishScreen = new SelectDishScreen($("#SelectDishScreen"));
+	var DishDetailsScreen = new DishDetailsScreen($("#DishDetailsScreen"));
+	var DinnerOverviewScreen = new DinnerOverviewScreen($("#DinnerOverviewScreen"));
+	var InstructionsScreen = new InstructionsScreen($("#InstructionsScreen"));
 
 });
