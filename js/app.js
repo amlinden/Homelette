23c59c9ae98f83858241fb2d35f66ec2a2cfb049
Alixred <alexaj@kth.se>
2017-03-21 11:26:40 +0100

    lagt till leftNavBarController

--- js/app.js
+++ js/app.js
@@ -4,13 +4,14 @@ $(function() {
 	var model = new DinnerModel();
 	
 	//And create the needed controllers and views
-	var exampleView = new ExampleView($("#exampleView"), model);
+	//var exampleView = new ExampleView($("#exampleView"), model);
 
 	//var HomeScreen = new HomeScreen($("#HomeScreen"));
 	var leftNavBar = new LeftNavBar($("#LeftNavBar"), model);
-	var selectDishScreen = new SelectDishScreen($("#SelectDishScreen"));
-	var dishDetailsScreen = new DishDetailsScreen($("#DishDetailsScreen"));
-	var dinnerOverviewScreen = new DinnerOverviewScreen($("#DinnerOverviewScreen"));
-	var preparationScreen = new PreparationScreen($("#PreparationScreen"));
+	var leftNavBarController = new LeftNavBarController(leftNavBar, model);
+	var selectDishScreen = new SelectDishScreen($("#SelectDishScreen"), model);
+	var dishDetailsScreen = new DishDetailsScreen($("#DishDetailsScreen"), model);
+	var dinnerOverviewScreen = new DinnerOverviewScreen($("#DinnerOverviewScreen"), model);
+	var preparationScreen = new PreparationScreen($("#PreparationScreen"), model);
 
 });
---
+++ js/controller/leftNavBarController.js
@@ -0,0 +1,11 @@
+//Controller constructor
+var LeftNavBarController = function (view, model) {
+
+	view.plusButton.click(function(){
+		model.setNumberOfGuests(model.getNumberOfGuests() + 1);
+	});
+
+	view.minusButton.click(function(){
+		model.setNumberOfGuests(model.getNumberOfGuests() - 1);
+	});
+}
